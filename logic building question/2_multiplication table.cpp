50e43978e224317e7619917c74d783a66f333617
Varun Thakur <vtthakurvarun@gmail.com>
2026-02-24 19:08:10 +0530

    doing some basic questions

---
+++ logic building question/2_multiplication table.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the number";
+    cin>>n;
+    for(int i=0;i<11;i++){
+        cout<<n<<"X"<<i<<"="<<n*i<<endl;
+          }
+    return 0;
+}
