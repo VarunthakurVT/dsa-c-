4f4c93ed9cad3eacf144ebfd899bfbbf95e647b4
Varun Thakur <vtthakurvarun@gmail.com>
2026-02-26 18:50:11 +0530

    revising some concept

--- algrothimns dsa/sub array kandanealgo/1_maximum number of sub array.cpp
+++ algrothimns dsa/sub array kandanealgo/1_maximum number of sub array.cpp
@@ -1,16 +1,16 @@
 #include<iostream>
-using namespace std; 
-int main(){
-    int arr[]={1,2,3,4,5};
+#include<vector>
+using namespace std;
+int main(){ 
     int n=5;
-    for(int st=0;st<n;st++){
-        for (int end=st;end<n;end++){
-            for(int i=st;i<end;i++){
+    int arr[5]={1,2,3,4,5}; //declare the array
+  for(int start=0;start<n;start++){
+     for(int end=start;end<n; end++){
+        for(int i=start;i<end;i++){
             cout<<arr[i];
-            }
-            cout<<" ";
         }
-        cout<<endl;
-    }
-return 0;
+        cout<<" ";
+     }
+  }
+  return 0;
 }
