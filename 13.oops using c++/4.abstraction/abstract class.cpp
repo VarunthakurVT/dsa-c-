3c22fff4bac8e274c1b7552aea60215ddca6d130
varun thakur <vtthakurvarun@gmail.com>
2026-03-24 10:37:56 +0530

    leaning some oops concept

--- 13.oops using c++/4.abstraction/abstract class.cpp
+++ 13.oops using c++/4.abstraction/abstract class.cpp
@@ -1,4 +1,5 @@
-#include<iostream>
+//abstract class have pure virtual function 
+#include<iostream> 
 using namespace std;
 class Shape { //abstract class
     public:
@@ -10,9 +11,26 @@ class Circle :public Shape{
         cout<<"drawCircle"<<endl;
     }
 };
+class square :public Shape{
+    public:
+    void shape(){
+        cout<<"drawSquare"<<endl;
+    }
+};
 int main(){
+    //this is call by value 
   Circle c1;
+  square s1;
+
   c1.shape();
+  s1.shape(); 
+  //this is call by function 
+  Shape* shape1 = new Circle(); 
+    Shape* shape2 = new square();
+
+    // The Magic Happens Here:
+    shape1->shape(); // Outputs: drawCircle
+    shape2->shape(); // Outputs: drawSquare
   return 0;
 
 }
