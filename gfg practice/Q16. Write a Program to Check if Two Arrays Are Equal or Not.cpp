cdc7da440b28f2ebac707b1cf2fa1033d2e43891
varun thakur <vtthakurvarun@gmail.com>
2026-04-18 20:01:53 +0530

    do the question but not complete and start the queue

---
+++ gfg practice/Q16. Write a Program to Check if Two Arrays Are Equal or Not.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std; 
+int main(){
+    int n =5;
+    int count;
+    int count1;
+    int arr[n]={1,2,3};
+    int arr2[n]={1,2,3};
+    for(int i=0; i<n;i++){
+        cout<<arr[i];
+        count++;
+    }
+    for(int j=0; j<n;j++){
+        cout<<arr[j];
+        if(j=n){
+       return count1++;
+    }}
+    cout<<count1;
+    
+    
+    if (arr==arr2){
+        cout<<"array 1 and array 2 are equal";
+    }
+    else {
+        cout<<"not equal ";
+    }
+    return 0;
+}
